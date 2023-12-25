03f2764300525a71b6465050cae03958c9747183
Sin3d <Sin3d@HOME-PC>
2023-12-25 15:46:42 +0300

    Добавьте файлы проекта.

---
+++ metod2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Metod2
+{
+    static void Main()
+    {
+        Queue<int> ocher = new Queue<int>();
+
+        string command;
+        while ((command = Console.ReadLine()) != "exit")
+        {
+            string[] parts = command.Split();
+            string operation = parts[0];
+
+            switch (operation)
+            {
+                case "push":
+                    try
+                    {
+                        int number = int.Parse(parts[1]);
+                        ocher.Enqueue(number);
+                        Console.WriteLine("ok");
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+                        // Console.WriteLine(e.ToString());
+                        Console.WriteLine("Некорректный ввод (вы ввели не число)");
+                        break;
+                    }
+
+                case "pop":
+                    if (ocher.Count > 0)
+                    {
+                        int firstElement = ocher.Dequeue();
+                        Console.WriteLine(firstElement);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка, невозможно удалить элемент так как очередь пуста");
+                    }
+                    break;
+
+                case "front":
+                    if (ocher.Count > 0)
+                    {
+                        int firstElement = ocher.Peek();
+                        Console.WriteLine(firstElement);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка, очередь пуста");
+                    }
+                    break;
+
+                case "size":
+                    Console.WriteLine(ocher.Count);
+                    break;
+
+                case "clear":
+                    ocher.Clear();
+                    Console.WriteLine("ok");
+                    break;
+                default:
+                    Console.WriteLine("Вы написали некорректную команду для очереди");
+                    break;
+            }
+        }
+
+        Console.WriteLine("bye");
+    }
+}
